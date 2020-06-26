67418aea99786cf1ac7c600d0d4ad6f627304857
Alfred Bullus <Alfred.Bullus@softwire.com>
2020-06-26 15:11:34 +0100

    Added better formatting plus a check for valid numbers

--- index.js
+++ index.js
@@ -1,39 +1,63 @@
 const readline = require('readline-sync');
-console.log('Welcome to the calculator!');
-var Output
 
-var Numbers = []
-
-console.log('Please enter the operator: ');
-const Operator = readline.prompt();
-
-console.log('How many numbers do you want to '+Operator+'?')
-const Size = parseInt(readline.prompt())
-
-for (let i = 0; i < Size; i++) {
-    console.log('Please enter number '+(i+1));
-    Numbers[i] = parseFloat(readline.prompt());
+function printWelcomeMessage (){
+    console.log('Welcome to the calculator!');
+    console.log("============================")
 }
 
-Output  = Numbers[0]
-for (let j = 1; j < Size; j++){
-    switch (Operator) {
-        case '+':
-            Output += Numbers[j];
-            break;
-        case '-':
-            Output -= Numbers[j];
-            break;
-        case '*':
-            Output *= Numbers[j];
-            break;
-        case '/':
-            Output /= Numbers[j];
-            break;
-        default:
-            console,log("Sorry, I don't understand ${Operator}.")
+function getANumber (){
+    while (true){
+        Input = readline.prompt();
+        if (isNaN(Input)){
+            console.log("Please enter a number: ")
+        } else{
+            return parseFloat(Input)
+        }
     }
 }
 
+function getInputs(){
+    let Numbers = []
+    console.log('Please enter the operator: ');
+    const Operator = readline.prompt();
+    
+    console.log('How many numbers do you want to '+Operator+' ?')
+    const Size = getANumber();
+    
+    for (let i = 0; i < Size; i++) {
+        console.log('Please enter number '+(i+1));
+        Numbers[i] = getANumber();
+    }
+    return {
+        Operator, Size, Numbers
+    }
+}
 
-console.log(Output);
+function performOneCalculation(){
+    let {Operator, Size, Numbers} = getInputs()
+    let Output  = Numbers[0]
+    for (let j = 1; j < Size; j++){
+        switch (Operator) {
+            case '+':
+                Output += Numbers[j];
+                break;
+            case '-':
+                Output -= Numbers[j];
+                break;
+            case '*':
+                Output *= Numbers[j];
+                break;
+            case '/':
+                Output /= Numbers[j];
+                break;
+            default:
+                console.log("Sorry, I don't understand ${Operator}.")
+        }
+    }    
+    console.log(Output);
+}
+
+printWelcomeMessage()
+while (true) {
+    performOneCalculation()
+}
