396ff98facef80afcf5da14fcf0b33bcd1a75742
Alfred Bullus <Alfred.Bullus@softwire.com>
2020-06-26 13:21:09 +0100

    Added iteration

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const readline = require('readline-sync');
 console.log('Welcome to the calculator!');
+var Output
 
+var Numbers = []
 
-console.log('Please enter the first number:');
-const Number1 = parseInt(readline.prompt());
-console.log('Please enter the second number: ')
-const Number2 = parseInt(readline.prompt());
+console.log('Please enter the operator: ');
+const Operator = readline.prompt();
 
-Output  =  Number1 * Number2
+console.log('How many numbers do you want to '+Operator+'?')
+const Size = parseInt(readline.prompt())
+
+for (let i = 0; i < Size; i++) {
+    console.log('Please enter number '+(i+1));
+    Numbers[i] = parseFloat(readline.prompt());
+}
+
+Output  = Numbers[0]
+for (let j = 1; j < Size; j++){
+    switch (Operator) {
+        case '+':
+            Output += Numbers[j];
+            break;
+        case '-':
+            Output -= Numbers[j];
+            break;
+        case '*':
+            Output *= Numbers[j];
+            break;
+        case '/':
+            Output /= Numbers[j];
+            break;
+        default:
+            console,log("Sorry, I don't understand ${Operator}.")
+    }
+}
+
 
 console.log(Output);
